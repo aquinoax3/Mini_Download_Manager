c1f973f685cff4952c1fba5db9dacb27fac3a6c7
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-04-16 23:56:24 -0400

    using steady_clock to get begin and end time to log out duration, commented out time_t

--- downloadFile.cpp
+++ downloadFile.cpp
@@ -5,12 +5,14 @@
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 
 
 std::mutex coutMutex;
+
 int count = 0;
-time_t timestamp;
+// time_t timestamp;
 
 
 void downloadFile(std::string fileName) {
@@ -18,10 +20,12 @@ void downloadFile(std::string fileName) {
     int delay = rand() % 1000 + 500; // 500ms to 1500ms
     std::string progressBar = "----------";
     int left = 0;
-    time(&timestamp);
-    std::cout << "TIME: " << ctime(&timestamp) << std::endl;
-    // const auto startTime = std::chrono::steady_clock::now();
+    auto begin = std::chrono::steady_clock::now();
     
+    // time(&timestamp);
+    // std::cout << "TIME: " << ctime(&timestamp) << std::endl;
+    // const auto startTime = std::chrono::steady_clock::now();
+
     for (int i = 0; i <= 100; i += 10) {
         // std::lock_guard<std::mutex> lock(coutMutex);
         // TODO: Add Progress Bar filling up
@@ -36,8 +40,41 @@ void downloadFile(std::string fileName) {
         left++;
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
         
-        // const auto endTime = std::chrono::steady_clock::now();
+        auto endTime = std::chrono::steady_clock::now();
+        std::cout << "DURATION: " << std::chrono::duration_cast<std::chrono::seconds>(endTime - begin).count() << "s\n";
+
         // const std::chrono::duration<double, std::milli> elapsedTime = endTime - startTime;
     }
 
 }
+
+
+// int longestSubArr (std::vector<int> arr, int k) {
+//     int left = 0;
+//     int currSum = 0;
+//     int result = 1;
+
+//     for (int right = 0; right < arr.size(); right++) {
+//         currSum += arr[right];
+//         std::cout << currSum << std::endl;
+
+//         while (currSum > k) {
+//             currSum -= arr[left];
+//             left++;
+//         }
+
+//         result = std::max(result, right - left + 1);
+//     }
+
+//     return result;
+// }
+
+
+// int main() {
+
+//     std::vector<int> numArr = {3, 1, 2, 7, 4, 2, 1, 1, 5};
+
+//     std::cout << longestSubArr(numArr, 8);
+
+//     return 0;
+// }
